19350b20766d49db44208555615d84f072fe267d
Johnny Howe <jonathon.o.howe@gmail.com>
2021-08-05 07:09:58 +1200

    Running character

---
+++ Assets/Player/PlayerAnimatorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerAnimatorController : MonoBehaviour
+{
+    public float runVelocityThreshold = 0.1f;
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+        rb2d = GetComponent<Rigidbody2D>(); 
+        renderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = Mathf.Abs(rb2d.velocity.x) > runVelocityThreshold;
+        animator.SetBool("running", isRunning);
+        if (isRunning)
+        {
+            renderer.flipX = rb2d.velocity.x < 0;
+        }
+    }
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float runAcceleration = 1;
+    public float maxRunSpeed = 1;
+
+    float horizontal = 0;
+    bool jump = false;
+
+    Rigidbody2D _rb;
+
+    void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetKey("space");
+    }
+
+    void FixedUpdate() {
+        _rb.velocity = new Vector2(Mathf.Clamp(_rb.velocity.x + runAcceleration * Time.deltaTime * horizontal, - maxRunSpeed, maxRunSpeed), _rb.velocity.y);
+    }
+}
