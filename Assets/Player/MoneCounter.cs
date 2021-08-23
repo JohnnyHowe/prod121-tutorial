0477be0e5716edb58b810b17dddb8ce29495b6b2
Johnny Howe <jonathon.o.howe@gmail.com>
2021-08-23 15:53:43 +1200

    Mone, respawning

---
+++ Assets/Player/MoneCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneCounter : MonoBehaviour
+{
+    private int mone = 0;
+    private bool destroyedThisFrame = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Mone" && !destroyedThisFrame) {
+            destroyedThisFrame = true;
+            mone ++;
+            Debug.Log("Mone: " + mone);
+            Destroy(other.gameObject);
+        } 
+    }
+
+    void FixedUpdate() {
+        destroyedThisFrame = false;
+    }
+}
---
+++ Assets/Scripts/RespawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player") {
+            other.gameObject.transform.position = respawnPoint.position;
+        }
+    }
+}
